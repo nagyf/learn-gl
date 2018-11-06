58dfa84b06b3c5e42a3c24f075d3104e41e5806a
Ferenc Nagy <ferenc.nagy1@ge.com>
2018-11-06 09:21:44 +0100

    Initial Commit

---
+++ learn-gl/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  learn-gl
+//
+//  Created by Ferenc Nagy on 2018. 11. 06..
+//  Copyright © 2018. Ferenc Nagy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
